15b4a4cf41be37d36b6bd0beeb4495c3889dc9d9
Remix Workspace <>
2022-10-22 16:48:52 +0100

    global variables in solidity

--- Varaiable.sol
+++ Varaiable.sol
@@ -2,9 +2,17 @@
 pragma solidity 0.8.7;
 
 contract Variable {
-    uint public stateVariable = 3 ;
+    // uint public stateVariable = 3 ;
 
-    function foo() external pure returns(uint) {
-        uint localVariable = 4 ;
+    // function foo() external pure returns(uint) {
+    //     uint localVariable = 4 ;
+    // }
+
+    function golobalVars() external view returns(address , uint , uint){
+        address myAddress = msg.sender ;
+        uint timestamp = block.timestamp;
+        uint blockNum = block.number;
+
+        return (myAddress , timestamp , blockNum);
     }
 }

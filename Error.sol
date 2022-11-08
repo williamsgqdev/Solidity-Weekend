40cdc4adf4e4c50cac7786789ea83b73cf032617
williamgqdev <williamsagbunu@gmail.com>
2022-11-08 11:49:47 +0100

    Errors in solidity

---
+++ Error.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
+
+
+contract ErrorHandler {
+  function testrequire(uint _i) public pure {
+    require(_i <= 10 , 'i > 10');
+  }
+
+  function testRevert(uint _i) public pure {
+      if(_i > 10){
+          revert("i > 10");
+      }
+  }
+}
